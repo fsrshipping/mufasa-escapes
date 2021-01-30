501ca6f2c705bd3b056c1e7e5429566618f4aaa4
fsrshipping <74324252+fsrshipping@users.noreply.github.com>
2021-01-30 20:45:01 +0100

    Update script-reservation.js

--- script/script-reservation.js
+++ script/script-reservation.js
@@ -5,5 +5,5 @@ check.addEventListener("click", checkRes);
 
 function checkRes() {
     let reservationr = document.getElementById("reservationnumber").value;
-    window.location=`http://mufasa-escapes.netlify.app/booking/${reservationr}.html`;
+    window.location=`http://mufasa-escapes.herokuapp.com/booking/${reservationr}.html`;
 }
