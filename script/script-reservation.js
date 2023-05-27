91cc54760d3105ffd70b0ea6458800a80fc576d6
fsrshipping <74324252+fsrshipping@users.noreply.github.com>
2023-05-27 17:20:58 +0200

    Delete script directory

--- script/script-reservation.js
+++
@@ -1,9 +0,0 @@
-const check = document.getElementById("reservation-check");
-
-
-check.addEventListener("click", checkRes);
-
-function checkRes() {
-    let reservationr = document.getElementById("reservationnumber").value;
-    window.location=`http://mufasa-escapes.vercel.app/booking/${reservationr}.html`;
-}
